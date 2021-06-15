44a56e9c6c8173d2aa264c72c83760a66a92fd13
Vlad Basoon <vbasoon@gmail.com>
2021-06-15 08:32:07 +0300

    cart loyaut

---
+++ src/components/products/CardItemCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>CartItemCard</h1>
+        <p>Category</p>
+        <p>Name</p>
+        <p>Price</p>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,9 +1,27 @@
 <template>
     <div>
-        <h1 class="text-3xl">This is a Cart</h1>
+        <h1 class="text-3xl">Your Cart</h1>
+        <div v-for="product in items" :key="product.id">
+            {{ product.name }}
+        </div>
+
+        <CartItemCard />
     </div>
 </template>
 
-<script></script>
+<script>
+import CartItemCard from '../components/products/CardItemCard.vue'
+export default {
+    name: 'Cart',
+    components: {
+        CartItemCard,
+    },
+    computed: {
+        items() {
+            return this.$store.getters.cartItems
+        },
+    },
+}
+</script>
 
 <style></style>
